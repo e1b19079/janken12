af8cb01e07b439772028c54f751f0c84ddb85d18
yasumura-kaito <e1b19100@oit.ac.jp>
2021-10-05 15:02:02 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team12/janken12/Janken12Application.java
@@ -0,0 +1,13 @@
+package oit.is.team12.janken12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken12Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team12/janken12/Janken12ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team12.janken12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
